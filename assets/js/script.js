351e5edc3a35add6b76c3aac90aad3a5bd00f4d4
fabinho07 <87538278+fabinho07@users.noreply.github.com>
2024-05-06 10:03:26 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+/*
+function determinarSexo(nome) {
+    const nomesMasculinos = "paulo"
+    const nomesFemininos = "maria"
+  
+    if (nomesMasculinos.includes(nome)) {
+      return "Masculino";
+    } else if (nomesFemininos.includes(nome)) {
+      return "Feminino";
+    } else {
+      return "Indefinido";
+    }
+  }
+  
+  
+  console.log(determinarSexo("maria")); 
+*/
+
+const determinarGenero = (masculino) => {
+   const masc = "homem"
+   const fem = "mulher" 
+
+   if(determinarGenero == masculino)
+   return "homem"
+}
+
+
+console.log(determinarGenero)
+
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
